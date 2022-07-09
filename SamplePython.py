0e34d5f7543f3e01c0d45266b46213fd11ff9990
Rekha <iniyarasan@gmail.com>
2022-07-09 19:33:39 +0530

    created the sample class python code

---
+++ SamplePython.py
@@ -0,0 +1,17 @@
+class Dog():
+    def __init__(self,breed,name,spot):
+        self.breed = breed
+        self.name = name
+        self.spot = spot
+
+    def my_dog(self):
+        if self.spot:
+            print(f'My dog is of {self.breed} breed and we call him {self.name} also he has few spots!!!')
+        else:
+            print(f'My dog is of {self.breed} breed and we call him {self.name} also he has no spots!!!')
+
+# creating instance of the class
+
+my_obj = Dog('German Shepeard','Cecear',True)
+
+my_obj.my_dog()
